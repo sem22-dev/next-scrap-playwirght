cbdf4a1688984fe7db2782752a19ce5b02d40ff8
sem22-dev <thotjj22@gmail.com>
2024-03-06 01:51:44 +0530

    updated api

--- app/[username]/followerDisplay.tsx
+++ app/[username]/followerDisplay.tsx
@@ -14,7 +14,7 @@ export default function DisplayFollowers({ username }: { username: string }) {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(`https://54.196.45.15/user-details?username=${username}`);
+      const response = await fetch(`http://13.201.228.36/user-details?username=${username}`);
       const data = await response.json();
       setFollowerCount(data.followerCount);
       setProfilePicUrl(data.profilePicUrl);
